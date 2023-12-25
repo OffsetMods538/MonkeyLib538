5b36ab13e14ba9b4b8118934d375afeee3ed4e93
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-12-25 17:43:34 +0200

    [TESTMOD] Add testmod

---
+++ src/testmod/java/testmod/Testmod.java
@@ -0,0 +1,15 @@
+package testmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Testmod implements ModInitializer {
+    public static final String MOD_ID = "testmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
