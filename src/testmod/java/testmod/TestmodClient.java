e9c3c30037b9905197ecc41c53d94a6f9af3d382
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2024-01-01 15:39:55 +0200

    [CONFIG TESTMOD] Add config screen

--- src/testmod/java/testmod/TestmodClient.java
+++ src/testmod/java/testmod/TestmodClient.java
@@ -1,11 +1,12 @@
 package testmod;
 
 import net.fabricmc.api.ClientModInitializer;
+import testmod.config.ConfigTestmod;
 
 public class TestmodClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-
+        new ConfigTestmod().onInitializeClient();
     }
 }
--- src/testmod/java/testmod/config/ConfigTestmod.java
+++ src/testmod/java/testmod/config/ConfigTestmod.java
@@ -1,15 +1,21 @@
 package testmod.config;
 
 import top.offsetmonkey538.monkeylib538.config.ConfigManager;
+import top.offsetmonkey538.monkeylib538.config.MonkeyLib538ModMenu;
 
 import static testmod.Testmod.*;
 
 public class ConfigTestmod {
+    public static ModConfig config;
 
     public void onInitialize() {
-        final ModConfig config = ConfigManager.init(new ModConfig(), LOGGER::error);
+        config = ConfigManager.init(new ModConfig(), LOGGER::error);
 
         LOGGER.info("hello: " + config.hello);
         LOGGER.info("Number: " + config.Number);
+    }
+
+    public void onInitializeClient() {
+        MonkeyLib538ModMenu.registerConfigScreen(MOD_ID, ModConfigScreen::new);
     }
 }
