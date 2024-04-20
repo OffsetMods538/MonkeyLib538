72b895f251d19f31e9163da4a658dd1633584248
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2024-04-20 21:06:58 +0300

    Add setResourcePackProperties

---
+++ src/main/java/top/offsetmonkey538/monkeylib538/mixin/ServerPropertiesHandlerMixin.java
@@ -0,0 +1,40 @@
+package top.offsetmonkey538.monkeylib538.mixin;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.dedicated.ServerPropertiesHandler;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+
+import top.offsetmonkey538.monkeylib538.duck.ServerPropertiesHandlerDuck;
+
+import java.util.Optional;
+
+@Mixin(ServerPropertiesHandler.class)
+public abstract class ServerPropertiesHandlerMixin implements ServerPropertiesHandlerDuck {
+
+    @Shadow
+    private static Optional<MinecraftServer.ServerResourcePackProperties> getServerResourcePackProperties(String id, String url, String sha1, @Nullable String hash, boolean required, String prompt) {
+        return Optional.empty();
+    }
+
+    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+    @Mutable
+    @Shadow @Final public Optional<MinecraftServer.ServerResourcePackProperties> serverResourcePackProperties;
+
+    @Override
+    public void monkeylib538$setResourcePackProperties(String url, String sha1Hash) {
+        final ServerPropertiesHandler thiz = ((ServerPropertiesHandler) (Object) this);
+
+        this.serverResourcePackProperties = getServerResourcePackProperties(
+                thiz.getString("resource-pack-id", ""),
+                url,
+                sha1Hash,
+                "",
+                thiz.parseBoolean("require-resource-pack", false),
+                thiz.getString("resource-pack-prompt", "")
+        );
+    }
+}
