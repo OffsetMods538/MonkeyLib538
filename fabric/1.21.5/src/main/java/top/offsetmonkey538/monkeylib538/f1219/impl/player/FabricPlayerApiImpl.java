1289f3730fce14b2f8813f8e808cee2384ee6fe3
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2025-12-14 20:01:40 +0200

    Add 1.21.5 version

---
+++ fabric/1.21.5/src/main/java/top/offsetmonkey538/monkeylib538/f1219/impl/player/FabricPlayerApiImpl.java
@@ -0,0 +1,13 @@
+package top.offsetmonkey538.monkeylib538.f1219.impl.player;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.PlayerManager;
+import org.jetbrains.annotations.NotNull;
+import top.offsetmonkey538.monkeylib538.fabric.api.player.FabricPlayerApi;
+
+public final class FabricPlayerApiImpl implements FabricPlayerApi {
+    @Override
+    public boolean isPlayerOpImpl(@NotNull PlayerManager playerManager, @NotNull PlayerEntity player) {
+        return playerManager.isOperator(player.getGameProfile());
+    }
+}
