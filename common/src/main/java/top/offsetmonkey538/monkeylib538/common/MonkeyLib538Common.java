e5653067054f62930883b0296b78596098f25323
OffsetMonkey538 <offsetmonkey538@proton.me>
2026-01-26 20:19:00 +0200

    Remove logging api, update offsetutils

--- common/src/main/java/top/offsetmonkey538/monkeylib538/common/MonkeyLib538Common.java
+++ common/src/main/java/top/offsetmonkey538/monkeylib538/common/MonkeyLib538Common.java
@@ -1,13 +1,13 @@
 package top.offsetmonkey538.monkeylib538.common;
 
-import top.offsetmonkey538.monkeylib538.common.api.log.MonkeyLibLogger;
+import top.offsetmonkey538.offsetutils538.api.log.OffsetLogger;
 import top.offsetmonkey538.monkeylib538.common.telemetry.TelemetryHandler;
 
 import java.util.ServiceLoader;
 
 public final class MonkeyLib538Common {
     public static final String MOD_ID = "monkeylib538";
-    public static final MonkeyLibLogger LOGGER = MonkeyLibLogger.create(MOD_ID);
+    public static final OffsetLogger LOGGER = OffsetLogger.create(MOD_ID);
 
     public static void initialize() {
         TelemetryHandler.initialize();
