1f61f0f2b091e2e3e9610a5bbefdaaf4be6e349f
OffsetMonkey538 <offsetmonkey538@proton.me>
2026-02-01 19:12:44 +0200

    My very cool stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,37 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import starlightSocialIcons from './src/utils/socialIcons.ts';
 import starlight from '@astrojs/starlight';
+
+import aiRobotsTxt from 'astro-ai-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
+	site: process.env.CUSTOM_SITE_ENV || undefined,
+	base: process.env.CUSTOM_BASE_ENV || undefined,
 	integrations: [
+		starlightSocialIcons({
+			modrinth: "https://modrinth.com/mod/monkeylib538"
+		}),
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'MonkeyLib538',
+			credits: true,
+			logo: {
+				src: './src/assets/face.svg'
+			},
+			customCss: [
+				'./src/styles/custom.css'
+			],
+			components: {
+				SocialIcons: './src/utils/SocialIcons.astro',
+				PageFrame: './src/utils/PageFrame.astro'
+			},
+			social: [
+				{icon: 'github', label: 'GitHub', href: 'https://github.com/OffsetMods538/MonkeyLib538'},
+				{icon: 'discord', label: 'Discord', href: 'https://discord.offsetmonkey538.top'}
+			],
 			sidebar: [
+				{label: 'Index', slug: ''},
 				{
 					label: 'Guides',
 					items: [
@@ -22,5 +45,6 @@ export default defineConfig({
 				},
 			],
 		}),
-	],
+		aiRobotsTxt()
+	]
 });
---
+++ src/content/docs/index.md
@@ -0,0 +1,17 @@
+---
+title: MonkeyLib538
+template: splash
+hero:
+  tagline: A library for OffsetMonkey538's mods that allows for multiversion and -loader coolness
+  image:
+    file: ../../assets/icon.svg
+  actions:
+    - text: View on Modrinth
+      link: https://modrinth.com/mod/monkeylib538
+      icon: external
+      variant: minimal
+    - text: Chat with me on Discord
+      link: https://discord.offsetmonkey538.top
+      icon: discord
+      variant: minimal
+---
